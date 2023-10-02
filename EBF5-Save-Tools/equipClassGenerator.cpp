32933fc8d74726264b95bdd2bb3373371f2fe435
QJrocks <qjrocksgames@gmail.com>
2023-10-02 03:50:20 -0400

    Updated the class generator and general files to properly scale with file sizes, meaning that icons can now be smaller!

--- EBF5-Save-Tools/equipClassGenerator.cpp
+++ EBF5-Save-Tools/equipClassGenerator.cpp
@@ -7,11 +7,11 @@ int main()
 {
     const int iconSize = 80;
     std::array<std::string, 11> names = { "sword", "staff", "gun", "bow", "toy", "hatm", "armorm", "hatf", "armorf", "flair" };
-    std::array<int, 11> amounts =       { 21,      21,      21,    21,    21,    21,     21,       21,     21,       55 };
+    std::array<int, 11> amounts = { 21,      21,      21,    21,    21,    21,     21,       21,     21,       55 };
     for (int y = 0; y < 11; y++) {
         for (int x = 0; x < amounts[y]; x++) {
             std::cout << "." << names[y] << x << " {" << '\n';
-            std::cout << "background-position: -" << ((x % 21) * iconSize) << "px -" << ((x / 21) * iconSize + (y * iconSize)) << "px;" << '\n';
+            std::cout << "background-position: calc(var(--equip-size) * -" << (x % 21) << ") calc(var(--equip-size) * -" << (x / 21) + y << ");" << '\n';
             std::cout << "}" << '\n';
         }
     }
